f5218282c3b35bf5a0a9be2e6e5810448c176eed
sonicer105 <sonic.ert@gmail.com>
2017-05-11 16:29:52 -0700

    Started work on the game part of the game (client side)

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,12 @@ let express = require('express'),
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home/index', { title: 'LTT Game', navSection: 0 });
+    res.render('home/index', { title: 'LTT Game', navSection: 0 });
+});
+
+/* GET game page */
+router.get('/game', function(req, res, next) {
+    res.render('home/game', { title: 'LTT Game', navSection: 1 });
 });
 
 module.exports = router;
